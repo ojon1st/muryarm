9d255b0752186a5e316dd74356deccd9a6193c2e
MOUSSA DAN SAADOU Abdourahamane <dev.abdourahamane.227@gmail.com>
2020-04-01 12:35:09 +0200

    Version 1.0.2

--- configs/config.js
+++ configs/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     DATABASE: process.env.NODE_ENV == 'production'
-        ? ''
+        ? 'mongodb+srv://db_user_for_dev:44fg3PHa@muryarmapp-8uovr.mongodb.net/test?retryWrites=true&w=majority'
         : 'mongodb://localhost:27017,localhost:27018,localhost:27019/muryarmatassa?replicaSet=rs',
 
     CAS_CONFIRMES: 22,
